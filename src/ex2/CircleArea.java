91a531f27749524b65855cb118ea49567048851b
Nestor162 <nestorcicardini@gmail.com>
2024-01-16 14:40:01 +0100

    Solve exercise number 2

---
+++ src/ex2/CircleArea.java
@@ -0,0 +1,16 @@
+package ex2;
+import java.util.Scanner;
+
+public class CircleArea {
+	
+	public static void main(String[] args) {
+		Scanner myScanner = new Scanner(System.in);
+		
+		System.out.println("Enter the radius of the circle: ");
+		double radius = myScanner.nextDouble();
+		
+		double area = Math.PI * (Math.pow(radius, 2));
+		
+		System.out.println("The circle area is: " + area);
+	}
+}

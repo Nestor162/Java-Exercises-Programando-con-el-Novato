3e06ae61150d7daae69fa4220806238b2e007231
Nestor162 <nestorcicardini@gmail.com>
2024-01-21 15:30:54 +0100

    Add basic version of first excercise

--- src/ex1/BasicOperators.java
+++ src/ex1/BasicOperators.java
@@ -1,49 +1,19 @@
 package ex1;
 
-import java.util.Scanner;
-
 public class BasicOperators {
 	public static void main(String[] args) {
-		Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-	    System.out.print("Enter the first nuber: ");
-	    double n1 = Double.parseDouble(myObj.nextLine());  // Read user input
-	    System.out.print("Enter the second nuber: ");
-	    double n2 = Double.parseDouble(myObj.nextLine());  // Read user input
-	    myObj.close(); // Closes the Scanner object
-
-	    
-		char[] operators = {'+','-','/','*', '%'};
+		int n1 = 2;
+		int n2 = 5;
 		
-		for (char operator : operators) {
-			
-			double result = 0.0;
-			String operation = "";
-			
-			switch (operator) {
-			case '+': 
-				result = n1 + n2;
-				operation = "addition";
-				break;
-			case '-':
-				result = n1 - n2;
-				operation  = "subtraction";
-				break;
-			case '/':
-				result = n1 / n2;
-				operation = "division";
-				break;
-			case '%':
-				result  = n1 % n2;
-				operation =  "modulus";
-				break;
-			case '*':
-				result  = n1 * n2;
-				operation =  "multiplication";
-				break;
-			}
-			
-			System.out.println("The " + operation  + " between " + n1 + " and " +  n2  + " is equal to " + result);
-		}
-		
+		System.out.println("The ADDITION between " + n1 + " and "  + n2 + 
+				" is equal to: " + (n1+n2));
+		System.out.println("The SUBTRACITON between " + n1 + " and "  + n2 + 
+				" is equal to: " + (n1-n2));
+		System.out.println("The MULTIPLICATION between " + n1 + " and "  + n2 + 
+				" is equal to: " + (n1*n2));
+		System.out.println("The DIIVSION between " + n1 + " and "  + n2 + 
+				" is equal to: " + (Double.valueOf(n1)/n2));
+		System.out.println("The MODULO between " + n1 + " and "  + n2 + 
+				" is equal to: " + (n1%n2));
 	}
 }
---
+++ src/ex1/BasicOperators2.java
@@ -0,0 +1,49 @@
+package ex1;
+
+import java.util.Scanner;
+
+public class BasicOperators2 {
+	public static void main(String[] args) {
+		Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+	    System.out.print("Enter the first nuber: ");
+	    double n1 = Double.parseDouble(myObj.nextLine());  // Read user input
+	    System.out.print("Enter the second nuber: ");
+	    double n2 = Double.parseDouble(myObj.nextLine());  // Read user input
+	    myObj.close(); // Closes the Scanner object
+
+	    
+		char[] operators = {'+','-','/','*', '%'};
+		
+		for (char operator : operators) {
+			
+			double result = 0.0;
+			String operation = "";
+			
+			switch (operator) {
+			case '+': 
+				result = n1 + n2;
+				operation = "addition";
+				break;
+			case '-':
+				result = n1 - n2;
+				operation  = "subtraction";
+				break;
+			case '/':
+				result = n1 / n2;
+				operation = "division";
+				break;
+			case '%':
+				result  = n1 % n2;
+				operation =  "modulus";
+				break;
+			case '*':
+				result  = n1 * n2;
+				operation =  "multiplication";
+				break;
+			}
+			
+			System.out.println("The " + operation  + " between " + n1 + " and " +  n2  + " is equal to " + result);
+		}
+		
+	}
+}
